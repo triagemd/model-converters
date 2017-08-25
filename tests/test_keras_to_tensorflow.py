4735bce475875fb190f18b0fbb51e26566eb3561
Steven Normore <snormore@gmail.com>
2017-08-25 17:44:02 -0400

    Catch socket.error in the test serving check

--- tests/test_keras_to_tensorflow.py
+++ tests/test_keras_to_tensorflow.py
@@ -5,7 +5,6 @@ import socket
 import subprocess
 
 from tensorflow_serving_client import TensorflowServingClient
-from grpc.framework.interfaces.face.face import AbortionError
 
 from model_converters import KerasToTensorflow
 from ml_tools import load_image, get_model_spec
@@ -44,14 +43,14 @@ def restart_serving_container(model_name):
     subprocess.call(['docker-compose', 'restart', model_name])
     for port in MODEL_SERVING_PORTS.values():
         attempt = 0
-        while attempt <= 10:
+        while attempt <= 15:
             attempt += 1
             try:
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.connect(('localhost', port))
                 if len(s.recv(1)) > 0:
                     break
-            except OSError:  # ConnectionRefusedError but with Python 2 support
+            except socket.error:
                 pass
             time.sleep(5)
 
@@ -66,28 +65,19 @@ def assert_converted_model(tf_model_dir):
 
 def assert_model_serving(model_name, imagenet_dictionary, expected_top_5):
     model_spec = get_model_spec(model_name)
-    attempt = 1
-    while True:
-        try:
-            client = TensorflowServingClient('localhost', MODEL_SERVING_PORTS[model_name])
-            image_data = load_image('tests/fixtures/files/cat.jpg', model_spec.target_size,
-                                    preprocess_input=model_spec.preprocess_input)
-            result = client.make_prediction(image_data, 'image')
-            assert 'class_probabilities' in result
-            assert len(result['class_probabilities']) == 1
-            assert len(result['class_probabilities'][0]) == 1000
-            predictions = result['class_probabilities'][0]
-            predictions = list(zip(imagenet_dictionary, predictions))
-            predictions = sorted(predictions, reverse=True, key=lambda kv: kv[1])[:5]
-            predictions = [(label, float(score)) for label, score in predictions]
-            print(predictions)
-            assert predictions == expected_top_5
-            break
-        except AbortionError as e:
-            if attempt > 5:
-                raise
-            time.sleep(1)
-            attempt += 1
+    client = TensorflowServingClient('localhost', MODEL_SERVING_PORTS[model_name])
+    image_data = load_image('tests/fixtures/files/cat.jpg', model_spec.target_size,
+                            preprocess_input=model_spec.preprocess_input)
+    result = client.make_prediction(image_data, 'image')
+    assert 'class_probabilities' in result
+    assert len(result['class_probabilities']) == 1
+    assert len(result['class_probabilities'][0]) == 1000
+    predictions = result['class_probabilities'][0]
+    predictions = list(zip(imagenet_dictionary, predictions))
+    predictions = sorted(predictions, reverse=True, key=lambda kv: kv[1])[:5]
+    predictions = [(label, float(score)) for label, score in predictions]
+    print(predictions)
+    assert predictions == expected_top_5
 
 
 def test_convert_imagenet_inception_v3(temp_file, imagenet_dictionary):
