49e2469246040bb1f4554b1bf8e190034e313d05
Stephen <stephen@nowhere>
2018-03-08 14:00:09 -0500

    Parallelize tests in CI

--- tests/test_keras_to_tensorflow.py
+++ tests/test_keras_to_tensorflow.py
@@ -4,39 +4,15 @@ import shutil
 import socket
 import subprocess
 import numpy as np
-import sys
 
 from tempfile import NamedTemporaryFile
 from tensorflow_serving_client import TensorflowServingClient
 from model_converters import KerasToTensorflow
 from ml_tools import load_image
 from keras_model_specs import ModelSpec
-from keras_model_specs.model_spec import BASE_SPEC_NAMES
 
 
-MODEL_SERVING_PORTS = {
-    'inception_v3': 9001,
-    'mobilenet_v1': 9002,
-    'resnet50': 9003,
-    'xception': 9004,
-    'vgg16': 9005,
-    'vgg19': 9006,
-    'inception_resnet_v2': 9007,
-    'inception_v4': 9008,
-    'resnet152': 9009,
-    'densenet_121': 9010,
-    'densenet_169': 9011,
-    'densenet_201': 9012,
-    'nasnet_large': None,  # takes too long
-    'nasnet_mobile': 9013,
-}
-# no idea why the Python 2 CI won't run these models (they run out of memory)
-if sys.version_info[0] == 2:
-    MODEL_SERVING_PORTS['inception_v4'] = None
-    MODEL_SERVING_PORTS['nasnet_mobile'] = None
-    MODEL_SERVING_PORTS['resnet152'] = None
-    MODEL_SERVING_PORTS['inception_resnet_v2'] = None
-    MODEL_SERVING_PORTS['densenet_169'] = None
+MODEL_SERVING_PORT = 9001
 
 
 def assert_lists_same_items(list1, list2):
@@ -49,7 +25,7 @@ def cat_image(model_spec):
 
 
 def setup_model(name, model_path):
-    tf_model_dir = '.cache/models/%s' % (name, )
+    tf_model_dir = '.cache/model'
 
     model_spec = ModelSpec.get(name)
     model = model_spec.klass(weights='imagenet', input_shape=tuple(model_spec.target_size))
@@ -71,13 +47,13 @@ def setup_model(name, model_path):
 
 
 def start_serving_container(model_name):
-    subprocess.call(['docker-compose', 'up', '-d', model_name])
+    subprocess.call(['docker-compose', 'up', '-d', 'tf-serving'])
     attempt = 0
     while attempt <= 60:
         attempt += 1
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect(('localhost', MODEL_SERVING_PORTS[model_name]))
+            s.connect(('localhost', MODEL_SERVING_PORT))
             if len(s.recv(1)) > 0:
                 break
         except socket.error:
@@ -86,7 +62,7 @@ def start_serving_container(model_name):
 
 
 def kill_serving_container(model_name):
-    subprocess.call(['docker-compose', 'stop', model_name])
+    subprocess.call(['docker-compose', 'stop', 'tf-serving'])
 
 
 def assert_converted_model(tf_model_dir):
@@ -99,7 +75,7 @@ def assert_converted_model(tf_model_dir):
 
 def assert_model_serving(model_name, expected_scores):
     model_spec = ModelSpec.get(model_name)
-    client = TensorflowServingClient('localhost', MODEL_SERVING_PORTS[model_name])
+    client = TensorflowServingClient('localhost', MODEL_SERVING_PORT)
     result = client.make_prediction(cat_image(model_spec), 'image')
 
     assert 'class_probabilities' in result
@@ -109,22 +85,15 @@ def assert_model_serving(model_name, expected_scores):
     np.testing.assert_array_almost_equal(np.array(scores), np.array(expected_scores).flatten())
 
 
-def test_convert_tests_cover_all_model_types():
-    assert_lists_same_items(BASE_SPEC_NAMES, MODEL_SERVING_PORTS.keys())
+def test_converted_model_has_same_scores():
+    model_name = os.getenv('MODEL_NAME', 'mobilenet_v1')
 
+    with NamedTemporaryFile() as f:
+        temp_file = f.name
+        tf_model_dir, expected_scores = setup_model(model_name, temp_file)
+        KerasToTensorflow.convert(temp_file, tf_model_dir)
 
-def test_converted_models_have_same_scores():
-    for model_name in MODEL_SERVING_PORTS:
-        if MODEL_SERVING_PORTS[model_name] is None:
-            continue
-        print('Testing model ' + model_name)
-
-        with NamedTemporaryFile() as f:
-            temp_file = f.name
-            tf_model_dir, expected_scores = setup_model(model_name, temp_file)
-            KerasToTensorflow.convert(temp_file, tf_model_dir)
-
-            assert_converted_model(tf_model_dir)
-            start_serving_container(model_name)
-            assert_model_serving(model_name, expected_scores)
-            kill_serving_container(model_name)
+        assert_converted_model(tf_model_dir)
+        start_serving_container(model_name)
+        assert_model_serving(model_name, expected_scores)
+        kill_serving_container(model_name)
