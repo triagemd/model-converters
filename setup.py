de23566168447fff8fa678899e922292041848a4
Stephen <stephen@nowhere>
2019-07-04 12:00:30 -0400

    Try forcing tf 1.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='model-converters',
-    version='1.0.1',
+    version='1.1.0',
     description='Tools for converting Keras models for use with other ML frameworks.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
